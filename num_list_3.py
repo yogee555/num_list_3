82d01e6a5c5714098f2eb663b3da0a39a7681027
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-01-02 10:25:58 +0530

    Add files via upload

---
+++ num_list_3.py
@@ -0,0 +1,29 @@
+# List type
+L1 = [1,"poorna",3.14,True,5+9j]
+print(type(L1))
+
+# values
+print(L1[-1])
+print(L1[1:4])
+print(L1[0])
+
+# add
+L1.append("yogee")
+print(L1)
+
+# delete
+L1.pop( )
+print(L1)
+
+# reverse
+L1.reverse()
+print(L1)
+
+# insert
+L1.insert(3,"YOY")
+print(L1)
+
+# sort method
+k1 = ["mango","cheary","guava","banana"]
+k1.sort()
+print(k1)
